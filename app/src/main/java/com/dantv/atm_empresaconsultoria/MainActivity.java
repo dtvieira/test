76ff7dff551c881593649a71767652bb7ab71dac
Daniel Torres <dan_tv@hotmail.com>
2017-11-08 13:38:34 +0400

    Primeiro Commit

---
+++ app/src/main/java/com/dantv/atm_empresaconsultoria/MainActivity.java
@@ -0,0 +1,57 @@
+package com.dantv.atm_empresaconsultoria;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    private ImageView btnEmpresa;
+    private ImageView btnServicos;
+    private ImageView btnClientes;
+    private ImageView btnContato;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnEmpresa = (ImageView) findViewById(R.id.empresaId);
+        btnServicos = (ImageView) findViewById(R.id.servicosId);
+        btnClientes = (ImageView) findViewById(R.id.clientesId);
+        btnContato = (ImageView) findViewById(R.id.contatoId);
+
+
+        btnEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+            }
+        });
+
+        btnServicos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ServicoActivity.class));
+            }
+        });
+
+        btnClientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ClientesActivity.class));
+            }
+        });
+
+        btnContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ContatoActivity.class));
+            }
+        });
+
+
+    }
+}
